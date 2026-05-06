2ce7bc2f9e1c59ee16cc1bbac0a3911e775b79d3
Jessica Jones-Copeland <jessica@realityfabric.net>
2026-05-05 22:12:05 -0700

    Add nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,9 @@
+vim.o.number = true
+vim.o.smartcase = true
+vim.o.wrap = true
+vim.o.breakindent = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = false
+
+vim.cmd[[colorscheme tokyonight]]
